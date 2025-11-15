bc5c3ecd19092c477a41a181cd78a7c09a512885
arvanda <arvandza@webmail.umm.ac.id>
2025-11-15 22:50:38 +0700

    Fix SupabaseService Injection

--- lib/app/data/services/supabase_service.dart
+++ lib/app/data/services/supabase_service.dart
@@ -7,12 +7,12 @@ class SupabaseService extends GetxService {
 
   Future<SupabaseService> init() async {
     await dotenv.load(fileName: ".env");
-    
+
     await Supabase.initialize(
-      url: dotenv.env['https://ilcqvltujdofdaqywequ.supabase.co']!,
-      anonKey: dotenv.env['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsY3F2bHR1amRvZmRhcXl3ZXF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NjExNTAsImV4cCI6MjA3ODMzNzE1MH0._ahD35q4WFXk_UXP_aM8cnpus-kO3Yd9V9_z-n8rbPU']!,
+      url: dotenv.env['SUPABASE_URL']!,
+      anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
     );
-    
+
     client = Supabase.instance.client;
     return this;
   }
